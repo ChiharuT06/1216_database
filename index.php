d4327b54c510f3646a54239019acec6db599c3d6
ChiharuT06 <u.chiharu0909@gmail.com>
2022-12-14 14:30:18 +0900

    form作成

--- index.php
+++ index.php
@@ -32,6 +32,15 @@
                 <legend>今日はどんな１日だった？</legend>
                 <label>日付<input type="date" name="day"></label><br>
                 <label>ひとこと<textArea name="content" rows="4" cols="40"></textArea></label><br>
+                <label>星の数
+                  <select name="level">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </select>
+                </label><br>
                 <input type="submit" value="送信">
             </fieldset>
         </div>
