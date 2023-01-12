ad3d547d376f767d847954b0cc6842647e83ec25
ChiharuT06 <u.chiharu0909@gmail.com>
2023-01-12 22:13:58 +0900

    SQL更新

--- profile.sql
+++ profile.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: 127.0.0.1
--- 生成日時: 2022-12-15 14:15:44
+-- 生成日時: 2023-01-12 14:08:39
 -- サーバのバージョン： 10.4.27-MariaDB
 -- PHP のバージョン: 8.1.12
 
@@ -43,7 +43,30 @@ INSERT INTO `stars_table` (`id`, `indate`, `text`, `level`, `date`) VALUES
 (2, '2022-12-14 16:44:57', 'とても楽しかった', '3', '2022-12-15'),
 (3, '2022-12-14 18:25:58', '楽しかった', '1', '2022-12-15'),
 (4, '2022-12-14 20:15:31', '課題頑張った', '1', '2022-12-15'),
-(5, '2022-12-14 20:32:37', 'テスト', '2', '2022-12-08');
+(5, '2022-12-14 20:32:37', 'テスト', '2', '2022-12-08'),
+(7, '2023-01-03 19:22:52', 'テスト', '4', '2023-01-03');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `user_table`
+--
+
+CREATE TABLE `user_table` (
+  `id` int(12) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `lid` varchar(128) NOT NULL,
+  `lpw` varchar(64) NOT NULL,
+  `mng_flg` int(1) NOT NULL,
+  `life_flg` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `user_table`
+--
+
+INSERT INTO `user_table` (`id`, `name`, `lid`, `lpw`, `mng_flg`, `life_flg`) VALUES
+(1, 'テスト太郎', 'test1', 'test1', 0, 0);
 
 --
 -- ダンプしたテーブルのインデックス
@@ -56,6 +79,12 @@ ALTER TABLE `stars_table`
   ADD PRIMARY KEY (`id`);
 
 --
+-- テーブルのインデックス `user_table`
+--
+ALTER TABLE `user_table`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- ダンプしたテーブルの AUTO_INCREMENT
 --
 
@@ -63,7 +92,13 @@ ALTER TABLE `stars_table`
 -- テーブルの AUTO_INCREMENT `stars_table`
 --
 ALTER TABLE `stars_table`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- テーブルの AUTO_INCREMENT `user_table`
+--
+ALTER TABLE `user_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
